26aa4b3129a5d0e9e3e4c9de18295122b58bfccc
Jesus <jchancayauris@unsa.edu.pe>
2025-05-14 22:26:04 -0500

    Se implemento una funcion que genera los arreglos del peor caso(invertidos), de acuerdo al numero(tamaño) que se le pasa por consola

--- insertionSort/insertionSort.py
+++ insertionSort/insertionSort.py
@@ -1,6 +1,7 @@
 import time
 import matplotlib.pyplot as plt
 import random
+import sys
 
 # Insertion Sort
 def insertion_sort(arr):
@@ -12,10 +13,20 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
 
+def generar_arreglos_worst_case(n):
+    print(n)
+    result = []
+    for i in range (0, n):
+        lista = []
+        for j in range(i,-1,-1):
+            lista.append(j+1)
+        result.append(lista)
+    return result
 
 if __name__ == "__main__":
-    numbers_worst_case = [10,9,8,7,6,5,4,3,2,1]
-    print(numbers_worst_case)
-    insertion_sort(numbers_worst_case)
-    print(numbers_worst_case)
+    n = sys.argv[1]
+    #generar los arrays de tamaños recibidos por el computador
+    arreglos = generar_arreglos_worst_case(int(n))
+    print(arreglos)
+    #insertion_sort(numbers_worst_case)
 
